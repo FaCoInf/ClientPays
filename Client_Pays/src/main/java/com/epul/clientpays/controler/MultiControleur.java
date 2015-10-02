e0ab1224155420dfe2158d42424fabac6dbb9097
FaCoInf <corinne.fagno@gmail.com>
2015-10-02 21:26:36 +0200

    Développements : 	- formulaire et validation 	- ajout bootstrap & JQuery

--- Client_Pays/src/main/java/com/epul/clientpays/controler/MultiControleur.java
+++ Client_Pays/src/main/java/com/epul/clientpays/controler/MultiControleur.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.epul.clientpays.model.Pays;
 import com.epul.clientpays.tools.EnvoiMessageSOAP;
@@ -20,52 +24,87 @@ import com.epul.clientpays.tools.EnvoiMessageSOAP;
  */
 @Controller
 public class MultiControleur {
-	
-	private static final Logger logger = LoggerFactory.getLogger(MultiControleur.class);
-	
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(MultiControleur.class);
+
 	private static String operation = "getPays";
 	private static String destenvoi = "http://localhost:8080/WSPays/services/PaysService";
 	private static String destination = "http://pays"; // Nom du package
 
-	private static String pays ="France";
+	private static String pays = "France";
 
 	private static EnvoiMessageSOAP unAppel = new EnvoiMessageSOAP();
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
 		String formattedDate = dateFormat.format(date);
 		
-		try {
-			operation = "getPays";
-			unAppel.connexion();
-			unAppel.creationMessage(operation, pays, destination);
-//			model.addAttribute("pays",  unAppel.EmmissionReception(destenvoi, pays).toString());
-			ArrayList<Pays> paysRecept = (ArrayList<Pays>) unAppel.EmmissionReception(destenvoi, pays);
-			model.addAttribute("pays",  paysRecept.get(0));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+		model.addAttribute("serverTime", formattedDate);
 		
+		// Recuperation liste pays
 		try {
 			operation = "getListPays";
 			unAppel.connexion();
 			unAppel.creationMessage(operation, pays, destination);
-			ArrayList<Pays> listPaysRecept = (ArrayList<Pays>) unAppel.EmmissionReception(destenvoi, pays);
-			model.addAttribute("listPays",  listPaysRecept);
+			ArrayList<Pays> listPaysRecept = (ArrayList<Pays>) unAppel
+					.EmmissionReception(destenvoi, pays);
+			
+			model.addAttribute("listPays", listPaysRecept);
+			
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			model.addAttribute("MesErreurs", e.getMessage());
+			e.printStackTrace();
 		}
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}
-	
+
+	/**
+	 * Modifier Jouet
+	 */
+	@RequestMapping(value = "home.htm")
+	public ModelAndView modifierJouet(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String destinationPage = "Erreur";
+		
+		String paysSelect = request.getParameter("country");
+		request.setAttribute("paysSelect",paysSelect);
+		
+		// Recuperation liste pays & info sur pays select
+		try {
+			operation = "getListPays";
+			unAppel.connexion();
+			unAppel.creationMessage(operation, pays, destination);
+			ArrayList<Pays> listPaysRecept = (ArrayList<Pays>) unAppel
+					.EmmissionReception(destenvoi, pays);
+			
+			request.setAttribute("listPays", listPaysRecept);
+			
+			operation = "getPays";
+			unAppel.connexion();
+			unAppel.creationMessage(operation, pays, destination);
+			ArrayList<Pays> paysRecept = (ArrayList<Pays>) unAppel
+					.EmmissionReception(destenvoi, pays);
+			
+			request.setAttribute("pays", paysRecept.get(0));
+			
+		} catch (Exception e) {
+			request.setAttribute("MesErreurs", e.getMessage());
+			e.printStackTrace();
+		}
+
+		destinationPage = "/home";
+
+		return new ModelAndView(destinationPage);
+	}
+
 }
--- Client_Pays/src/main/webapp/WEB-INF/views/home.jsp
+++ Client_Pays/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,22 +1,95 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ page session="false"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!-- Auteurs : Corinne & Laura -->
 <html>
 <head>
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+<link href="resources/stylesheets/bootstrap/css/bootstrap.min.css"
+	rel="stylesheet" />
+<link href="resources/stylesheets/css/style.css" rel="stylesheet">
+<script src="resources/stylesheets/jquery-2.1.3.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Home</title>
+
+<script type="text/javascript">
+	function Chargement() {
+		var obj = document.getElementById("id_erreur");
+		if (obj.value != '')
+			alert('Erreur signalée  : "' + obj.value + "'");
+	}
+	
+	$(document).ready(function() {
+
+		// Selection des listes déroulantes
+		var paysSelect = "<c:out value='${paysSelect}' />";
+
+		if (paysSelect != "")
+			$('#country').val(paysSelect);
+	});
+</script>
 </head>
 <body>
-	<h1>Hello world!</h1>
+	<!-- Conteneur principal -->
+	<div class="container">
+		<h1>La population d'un pays</h1>
 
-	<P>The time on the server is ${serverTime}.</P>
-	
-	<select name="country">
-   <c:forEach items="${listPays}" var="paysItem">
-       <option value="${paysItem.nom}">${paysItem.nom}</option>
-   </c:forEach>
-</select>
+		<!-- 		Contenu -->
+		<section class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 contenu">
+			<input type="hidden" name="uneErreur" value="${MesErreurs}"
+				id="id_erreur">
+			<form class="form-horizontal" method="post"
+				action="home.htm" onsubmit="">
+				<input type="hidden" name="type" value="home"
+					id="type" /> <input type="hidden" name="action"
+					value="home.htm" />
 
-	<h1>${pays.nom}</h1>
-	<h2>${pays.nomCapital}</h2>
-	<h3>${pays.nbHabitants}</h3>
+				<!-- input fields -->
+				<div class="form-group">
+					<label class="control-label">Sélectionnez le pays que vous
+						souhaitez parmi la liste suivante :</label><select name="country" id="country">
+						<c:forEach items="${listPays}" var="paysItem">
+							<option value="${paysItem.nom}">${paysItem.nom}</option>
+						</c:forEach>
+					</select>
+				</div>
+
+				<!-- Boutons Ajouter/Reset -->
+				<div class="form-group">
+					<div class="col-sm-6 col-sm-offset-4 col-md-4 col-md-offset-5">
+						<button type="submit" name="valider"
+							class="btn btn-default btn-primary">Valider</button>
+					</div>
+				</div>
+			</form>
+		</div>
+		</section>
+			
+			
+			<c:if test="${pays != null}">
+		<section class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 contenu">
+			<h2>${pays.nom}</h2>
+			<table class="table table-bordered">
+				<!-- <CAPTION>Tableau des Stages</CAPTION> -->
+				<thead>
+					<tr>
+						<th class="col-md-6">Capitale</th>
+						<th class="col-md-6">Nombre d'habitants</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>${pays.nomCapital}</td>
+						<td>${pays.nbHabitants}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		</section>
+		</c:if>
+	</div>
 </body>
 </html>
